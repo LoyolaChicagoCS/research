a616f505360e6d3b9bae13f430ceda3bc8d2a0f5
George K. Thiruvathukal <gkt@cs.luc.edu>
2019-03-29 22:30:04 -0500

    research handbook moving to Sphinx RTD theme

--- xbuild.sh
+++
@@ -1,6 +0,0 @@
-[ -f ~/.env/sphinx/bin/activate ] && . ~/.env/sphinx/bin/activate
-
-make clean
-make CONFIG=bootstrap html
-make latexpdf
-make epub
--- xrsync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rsync -av \
-	--delete \
-	--exclude .htaccess \
-	$(pwd)/build/ \
-	research.cs.luc.edu:/var/www/vhosts/research.cs.luc.edu/htdocs/
-
